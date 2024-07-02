670bb63e6a40f8e349888d072932e1c7bb8da155
leon55490 <leon55490@gmail.com>
2024-07-02 02:21:08 -0500

    Ya se muestra la información del usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { FondoComponent } from './animacion/fondo/fondo.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IndexComponent } from './pagina/index/index.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FondoComponent, ReactiveFormsModule, IndexComponent, RouterLink],
+  imports: [RouterOutlet, FondoComponent, ReactiveFormsModule, IndexComponent, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/pagina/respuesta/respuesta.component.ts
+++ src/app/pagina/respuesta/respuesta.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-respuesta',
-  standalone: true,
-  imports: [],
   templateUrl: './respuesta.component.html',
-  styleUrl: './respuesta.component.scss'
+  styleUrls: ['./respuesta.component.scss']
 })
-export class RespuestaComponent {
+export class RespuestaComponent implements OnInit {
+  nombre: string;
+  correo: string;
+  numero: string;
+  mensaje: string;
 
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {
+    this.nombre = '';
+    this.correo = '';
+    this.numero = '';
+    this.mensaje = '';
+  }
+
+  ngOnInit() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const storedData = localStorage.getItem('form-data');
+      if (storedData) {
+        const formData = JSON.parse(storedData);
+        this.nombre = formData.nombre;
+        this.correo = formData.correo;
+        this.numero = formData.numero;
+        this.mensaje = formData.mensaje;
+        console.log('Datos almacenados:', formData);
+      } else {
+        console.log('No se encontraron datos en localStorage');
+      }
+    } else {
+      console.log('localStorage no está disponible');
+    }
+  }
+
+  volver() {
+    this.router.navigate(['index']);
+  }
 }
